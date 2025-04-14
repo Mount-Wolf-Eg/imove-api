06f5119fc97d11db6aa6cf66a1475fb0be0eaca3
MohamedHamdy <mohamedhamdysayed8@gmail.com>
2025-04-14 22:21:11 +0200

    CategoryMedicalEquipmentSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,14 +14,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            PermissionsTableSeeder::class,
-            RoleTableSeeder::class,
-            UsersTableSeeder::class,
-            VendorTypesTableSeeder::class,
-            RegionsTableSeeder::class,
-            CitiesTableSeeder::class,
-            CurrenciesTableSeeder::class
-        ]);
+        // $this->call([
+        //     PermissionsTableSeeder::class,
+        //     RoleTableSeeder::class,
+        //     UsersTableSeeder::class,
+        //     VendorTypesTableSeeder::class,
+        //     RegionsTableSeeder::class,
+        //     CitiesTableSeeder::class,
+        //     CurrenciesTableSeeder::class
+        // ]);
+        $this->call(CategoryMedicalEquipmentSeeder::class);
+
     }
 }
