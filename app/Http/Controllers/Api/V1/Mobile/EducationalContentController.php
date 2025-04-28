e8ae43aa29e13f53b611b73974b1f626bf4a23c1
MohamedHamdy11 <mohamedhamdysayed8@gmail.com>
2025-04-28 20:23:37 +0300

    fix api educational contents

--- app/Http/Controllers/Api/V1/Mobile/EducationalContentController.php
+++ app/Http/Controllers/Api/V1/Mobile/EducationalContentController.php
@@ -75,8 +75,9 @@ class EducationalContentController extends BaseApiController
     {
         try {
             $doctor = auth()->user()->doctor;
-            // return $doctor;
+            // return $doctor->id;
             $success = $this->contract->assignToConsultation($consultation, $request->validated()['content_ids'], $doctor->id);
+
             if (!$success) {
                 return $this->respondWithError( __('messages.Unauthorized: This consultation is not for you'), Response::HTTP_FORBIDDEN);
             }
