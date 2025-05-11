10864eef3f09c304aaa3b1ea0c2e55350dd31a5e
Elsayed Feteh <elsayed.feteh@yahoo.com>
2025-05-11 19:20:28 +0300

    handle subscription packages

--- app/Http/Controllers/Api/V1/Mobile/PatientPackageController.php
+++ app/Http/Controllers/Api/V1/Mobile/PatientPackageController.php
@@ -47,7 +47,7 @@ class PatientPackageController extends BaseApiController
     {
         try {
             $package      = Package::findOrFail(request('package'));
-            $subscription = $this->subscriptionContract->create(Arr::except($request->validated(), ['doctor_id']) + ['doctor_id' => $package->user_id, 'patient_id' => auth()->id()]);
+            $subscription = $this->subscriptionContract->create(Arr::except($request->validated(), ['doctor_id', 'patient_id']) + ['doctor_id' => $package->user_id, 'patient_id' => auth()->id()]);
             resolve(ConsultationContract::class)->create($request->validated(), [
                 'package_id' => $subscription->package_id,
                 'subscription_id' => $subscription->id,
