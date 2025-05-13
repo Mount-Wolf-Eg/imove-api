f35189191be338f7eda1564f219a90395a201a43
MohamedHamdy11 <mohamedhamdysayed8@gmail.com>
2025-05-13 15:39:31 +0300

    add_city_id_to_universities_table

--- app/Models/University.php
+++ app/Models/University.php
@@ -7,21 +7,29 @@ use App\Traits\SearchTrait;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Translatable\HasTranslations;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class University extends Model
 {
     use SoftDeletes, ModelTrait, SearchTrait, HasTranslations;
     public const ADDITIONAL_PERMISSIONS = [];
-    protected $fillable = ['name', 'is_active'];
+    protected $table = "universities";
+    protected $fillable = ['name', 'city_id', 'is_active'];
     protected array $filters = ['keyword'];
-    protected array $searchable = [];
+    protected array $searchable = ['name'];
     protected array $dates = [];
     public array $filterModels = [];
     public array $filterCustom = [];
     public array $translatable = ['name'];
 
     //---------------------relations-------------------------------------
-
+    public function city(): BelongsTo
+    {
+        return $this->belongsTo(City::class, 'city_id');
+    }
     //---------------------relations-------------------------------------
 
     //---------------------Scopes-------------------------------------
