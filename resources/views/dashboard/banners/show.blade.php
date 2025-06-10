f95694b18eecfe66c91e5774a7ccfa67d2c236d7
MohamedHamdy11 <mohamedhamdysayed8@gmail.com>
2025-06-10 13:40:28 +0300

    fix show image banner

--- resources/views/dashboard/banners/show.blade.php
+++ resources/views/dashboard/banners/show.blade.php
@@ -7,17 +7,17 @@
     <div class="row">
         <div class="col-md-10">
             <div class="card">
-                @if($banner->media && Str::endsWith($banner->media->asset_url, ['.mp4', '.webm', '.ogg']))
+                @if($banner->mainImage && Str::endsWith($banner->mainImage->asset_url, ['.mp4', '.webm', '.ogg']))
                     <video controls style="max-height:400px; width: 100%;" class="mx-auto d-block float-md-left mr-md-4">
-                        <source src="{{ $banner->media->asset_url }}" type="video/mp4">
+                        <source src="{{ $banner->mainImage->asset_url }}" type="video/mp4">
                          @lang('messages.The browser does not support video playback')
                     </video>
                 @else
-                    <img src="{{ $banner->media->asset_url ?? asset('assets/images/logo-sm.png') }}" class="card-img-top img-fluid mx-auto d-block float-md-left mr-md-4" style="max-height:400px;">
+                    <img src="{{ $banner->mainImage->asset_url ?? asset('assets/images/logo-sm.png') }}" class="card-img-top img-fluid mx-auto d-block float-md-left mr-md-4" style="max-height:400px;">
                 @endif
             </div>
-            
-           
+
+
         </div>
 
     </div>
