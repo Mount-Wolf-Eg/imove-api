c12af825a4dfcea59dae4f87e9f4f4eaa84dd888
Elsayed Feteh <elsayed.feteh@yahoo.com>
2025-06-11 21:11:12 +0300

    handle order doctors by slots

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -207,10 +207,10 @@ class Doctor extends Model
                     ->take(1)
                     ->with(['shifts' => function ($shiftQuery) {
                         $shiftQuery
-                            ->orderBy('doctor_schedule_day_shifts.from_time')
+                            ->orderBy('doctor_schedule_day_shifts.from_time');
                             // ->whereNotNull('parent_id')
                             // ->limit(1);
-                            ->take(1);
+                            // ->take(1);
                     }]);
             }]);
     }
