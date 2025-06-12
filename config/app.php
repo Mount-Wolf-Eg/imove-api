d3f8e076ce2f11a0eebf508fc85ee1f36e525b95
Elsayed Feteh <elsayed.feteh@yahoo.com>
2025-06-12 19:33:56 +0300

    handle order doctors by slots

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Imove Dashboard'),
+    'name' => env('APP_NAME', 'Virtual-Touch'),
 
     /*
     |--------------------------------------------------------------------------
